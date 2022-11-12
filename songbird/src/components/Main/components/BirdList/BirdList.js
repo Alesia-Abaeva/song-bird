9166fe1a27967b9954f1b8aff574229faa46c51d
Alesia <alesia.litvinenko@gmail.com>
2022-11-12 19:49:34 +0300

    refactor: edit styles uppercase

--- songbird/src/components/Main/components/BirdList/BirdList.js
+++ songbird/src/components/Main/components/BirdList/BirdList.js
@@ -10,17 +10,17 @@ export const renderBirdList = (number) => {
   const birdList = elementsCreate('section', 'bird-list');
 
   const birdContainer = elementsCreate('div', styles['bird-list__container']);
-  const birdListNAmes = elementsCreate('ul', styles['bird-list__name']);
+  const birdListNames = elementsCreate('ul', styles['bird-list__name']);
 
   for (let i = 0; i < BIRDS_DATA[number].length; i++) {
     const nameItem = elementsCreate('li', styles['bird-list__item']);
     nameItem.innerHTML = `<span class="name-item_chekpoint"></span>
     ${BIRDS_DATA[number][i].name}`;
 
-    birdListNAmes.append(nameItem);
+    birdListNames.append(nameItem);
   }
 
-  birdContainer.append(birdListNAmes);
+  birdContainer.append(birdListNames);
 
   const cardBird = renderCardBird(store);
 
